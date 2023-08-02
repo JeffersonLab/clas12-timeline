fd15afa604316ea1482dfc7c6ad9d839fd83e1d9
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-08-02 10:41:11 -0400

    doc: improve usage guide

--- qa-physics/releaseTimelines.sh
+++ qa-physics/releaseTimelines.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 # copy a locally deployed timeline to the release directory
 
+if [ -z "$CLASQA" ]; then
+  echo "ERROR: please source env.sh first" >&2
+  exit 100
+fi
+
 if [ $# -ne 2 ]; then
   echo """
   USAGE: $0 [dataset] [target_directory]
 
     Final release of [dataset] to directory [target_directory]
+    - [target_directory] should be relative to TIMELINEDIR
+    - Example:
+        $0 rga_inbending rga/pass1/qa
+        => relases dataset 'rga_inbending' QA timelines to the
+           directory $TIMELINEDIR/rga/pass1/qa
 
-    Example: $0 rga_inbending rga/pass1/qa
-
-    WARNING: be careful not to overwrite anything you shouldn't...
+    ------------------------------------------------------------
+    WARNING: be careful not to overwrite anything you shouldn't!
+    ------------------------------------------------------------
   """ >&2
   exit 101
-fi
-if [ -z "$CLASQA" ]; then
-  echo "ERROR: please source env.sh first" >&2
-  exit 100
 fi
 
 dataset=$1
