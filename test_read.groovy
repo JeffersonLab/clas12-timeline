d0d2252a9bf480a89e024b94dd770475f384ade7
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-08-04 16:05:04 -0400

    feat: test reading HIPO file(s)

---
+++ test_read.groovy
@@ -0,0 +1,27 @@
+// check if a HIPO file can be opened
+
+/*
+TODO:
+- find a better place to put this file
+- consider qa-physics/Tools.groovy, but move it to some top-level `utils` directory
+- make a method, which returns true/false, instead of exit codes
+*/
+
+
+import org.jlab.groot.data.TDirectory
+
+if(args.length==0) {
+  System.err.println("ERROR: specify one or more HIPO files to test")
+  System.exit(100)
+}
+
+args.each {
+  try {
+    def inTdir = new TDirectory()
+    inTdir.readFile(it)
+  } catch(Exception ex) {
+    System.err.println("\nERROR: cannot read HIPO file $it\n\nSTACK TRACE:\n============")
+    ex.printStackTrace()
+    System.exit(100)
+  }
+}
