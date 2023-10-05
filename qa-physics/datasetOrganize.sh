6883c6c0290bed8d93974d9335b6ca5c43b645ed
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-10-05 19:17:54 -0400

    fix: do not force transient `qa-physics` files to be in `qa-physics/` source directory

--- qa-physics/datasetOrganize.sh
+++ qa-physics/datasetOrganize.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-if [ $# -ne 2 ];then echo "USAGE: $0 [dataset] [input_dir]" >&2; exit 101; fi
+if [ $# -ne 3 ];then echo "USAGE: $0 [dataset] [input_dir] [output_dir]" >&2; exit 101; fi
 dataset=$1
 inputDir=$2
+outputDir=$3
 
 if [ -z "$TIMELINESRC" ]; then
   echo "ERROR: please source environ.sh first" >&2
@@ -12,8 +13,8 @@ if [ -z "$TIMELINESRC" ]; then
 fi
 
 # cleanup / generate new dataset subdirs
-OUTMON_DIR=$TIMELINESRC/qa-physics/outmon.${dataset}
-OUTDAT_DIR=$TIMELINESRC/qa-physics/outdat.${dataset}
+OUTMON_DIR=$outputDir/outmon
+OUTDAT_DIR=$outputDir/outdat
 for dir in $OUTMON_DIR $OUTDAT_DIR; do
   echo "clean $dir"
   mkdir -p $dir
