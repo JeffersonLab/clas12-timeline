58bb7999d3152150e96e8619564649c557c1990e
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-04-18 11:41:32 -0400

    test: debug RCDB communication link failures

--- monitoring/src/main/java/org/jlab/clas12/monitoring/monitor2p2GeV.java
+++ monitoring/src/main/java/org/jlab/clas12/monitoring/monitor2p2GeV.java
@@ -229,8 +229,10 @@ public class monitor2p2GeV {
 		if(!userTimeBased)choiceTracking=" using HIT BASED tracking";
 		System.out.println("Eb="+Ebeam+" (EB="+EB+") , run="+runNum+" , "+choiceTracking);
 		try {
+                  System.out.println("ci-debug 0");
 			Thread.sleep(10000);// in ms
 		}catch (Exception e) {
+                  System.out.println("ci-debug 1");
 			System.out.println(e);
 		}
 
@@ -242,16 +244,24 @@ public class monitor2p2GeV {
 		float tofvt1 = 0;
 		float tofvt2 = 300;
 		float rfPeriod = 4.008f;
+                  System.out.println("ci-debug 2");
         ccdb = new ConstantsManager();
+                  System.out.println("ci-debug 3");
         ccdb.init(Arrays.asList(new String[]{"/daq/tt/fthodo", "/calibration/eb/rf/config", "/calibration/eb/rf/offset"}));
+                  System.out.println("ci-debug 4");
         rfTable = ccdb.getConstants(runNum, "/calibration/eb/rf/config");
+                  System.out.println("ci-debug 5");
         if (rfTable.hasEntry(1, 1, 1)) {
+                  System.out.println("ci-debug 6");
             System.out.println(String.format("RF period from ccdb for run %d: %f", runNum, rfTable.getDoubleValue("clock", 1, 1, 1)));
             rfPeriod = (float) rfTable.getDoubleValue("clock", 1, 1, 1);
+                  System.out.println("ci-debug 7");
         }
         rf_large_integer = 1000;
+                  System.out.println("ci-debug 8");
 
 		rfTableOffset = ccdb.getConstants(runNum,"/calibration/eb/rf/offset");
+                  System.out.println("ci-debug 9");
 		if (rfTableOffset.hasEntry(1, 1, 1)){
 			rfoffset1 = (float)rfTableOffset.getDoubleValue("offset",1,1,1);
 			rfoffset2 = (float)rfTableOffset.getDoubleValue("offset",1,1,2);
