ff7c211c61a8e96a5174d2608b7d5f635c85f5d6
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-03-07 13:51:18 -0500

    style: fix indentation in `.java` files

--- monitoring/src/main/java/org/jlab/clas12/monitoring/trigger.java
+++ monitoring/src/main/java/org/jlab/clas12/monitoring/trigger.java
@@ -11,33 +11,33 @@ import org.jlab.io.base.DataBank;
  */
 public class trigger {
 
-    H1F bits;
+  H1F bits;
 
-    public trigger() {
-        bits = new H1F("bits",65,0,65);
-        bits.getDataX(0);
-        bits.getEntries();
-        bits.getMaximumBin();
-        bits.getAxis().getNBins();
+  public trigger() {
+    bits = new H1F("bits",65,0,65);
+    bits.getDataX(0);
+    bits.getEntries();
+    bits.getMaximumBin();
+    bits.getAxis().getNBins();
+  }
+
+  public void processEvent(DataEvent event){
+    DataBank bank = event.getBank("RUN::config");
+    if (bank.rows()>0) {
+      bits.fill(64);
+      long t = bank.getLong("trigger",0);
+      for (int i=0; i<64; ++i)
+        if ( 1 == ((t>>i)&1) )
+          bits.fill(i);
     }
+  }
 
-    public void processEvent(DataEvent event){
-        DataBank bank = event.getBank("RUN::config");
-        if (bank.rows()>0) {
-            bits.fill(64);
-            long t = bank.getLong("trigger",0);
-            for (int i=0; i<64; ++i)
-                if ( 1 == ((t>>i)&1) )
-                    bits.fill(i);
-        }
-    }
-
-    public void write(String outputDir, int runNumber) {
-		TDirectory dir = new TDirectory();
-		dir.mkdir("/TRIGGER/");
-		dir.cd("/TRIGGER/");
-        dir.addDataSet(bits);
-        dir.writeFile(outputDir + String.format("/out_TRIGGER_%d.hipo",runNumber));
-	}
+  public void write(String outputDir, int runNumber) {
+    TDirectory dir = new TDirectory();
+    dir.mkdir("/TRIGGER/");
+    dir.cd("/TRIGGER/");
+    dir.addDataSet(bits);
+    dir.writeFile(outputDir + String.format("/out_TRIGGER_%d.hipo",runNumber));
+  }
 
 }
