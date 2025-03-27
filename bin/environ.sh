23fccfe7015bc107ccf4efea73554b40844999ef
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-03-27 13:04:51 -0400

    style: snake case

--- bin/environ.sh
+++ bin/environ.sh
@@ -8,8 +8,8 @@ printWarning() { echo -e "\e[1;35m[WARNING]: $* \e[0m" >&2; }
 log_config=logging # set to 'logging' for quiet, or to 'debug' for verbose
 
 # get the source code directory
-[ -z "${BASH_SOURCE[0]}" ] && thisEnv=$0 || thisEnv=${BASH_SOURCE[0]}
-export TIMELINESRC=$(realpath $(dirname $thisEnv)/..)
+[ -z "${BASH_SOURCE[0]}" ] && this_env=$0 || this_env=${BASH_SOURCE[0]}
+export TIMELINESRC=$(realpath $(dirname $this_env)/..)
 
 # RCDB
 [ -z "${RCDB_CONNECTION-}" ] && export RCDB_CONNECTION=mysql://rcdb@clasdb.jlab.org/rcdb
@@ -33,7 +33,7 @@ export TIMELINE_JAVA_OPTS="${timeline_java_opts[*]}"
 export TIMELINE_GROOVY_OPTS="${timeline_groovy_opts[*]}"
 
 # cleanup vars
-unset thisEnv
+unset this_env
 unset log_config
 unset timeline_java_opts
 unset timeline_groovy_opts
