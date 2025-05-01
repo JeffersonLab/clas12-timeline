b8583836300b4da66b11044a05c9cb442a8e7b0e
Sangbaek <sangbaek.lee@anl.gov>
2025-05-01 18:41:19 -0400

    Fit only once

--- src/main/java/org/jlab/clas/timeline/fitter/ALERTFitter.groovy
+++ src/main/java/org/jlab/clas/timeline/fitter/ALERTFitter.groovy
@@ -20,10 +20,10 @@ class ALERTFitter{
 		f1.setOptStat("1111");
 		double maxz = h1.getBinContent(h1.getMaximumBin());
 		f1.setRange(h1.getMaximumBin() - 50, h1.getMaximumBin() + 50);
-		f1.setParameter(0,maxz);
+		f1.setParameter(0,maxz-h1.getBinContent(0));
 		f1.setParameter(1, h1.getMaximumBin() + 0.5);
-		f1.setParameter(2, 20);
-		f1.setParameter(3, 0.0);
+		f1.setParameter(2, 5.0);
+		f1.setParameter(3, h1.getBinContent(0));
 		if (maxz>0) f1.setParLimits(0, maxz*0.9,maxz*1.1);
 		f1.setParLimits(3, 0.0, 0.1*maxz);
 
@@ -31,19 +31,19 @@ class ALERTFitter{
 
 		DataFitter.fit(f1, h1, "");
 
-		def makefit = {func->
-			hMean = func.getParameter(1)
-			hRMS  = func.getParameter(2).abs()
-			func.setRange(Math.max(hMean-2.5*hRMS, h1.getMaximumBin() - 50),Math.min(hMean+2.5*hRMS, h1.getMaximumBin() + 50))
-      func.setParLimits(0, func.getParameter(0)*0.9,func.getParameter(0)*1.1);
-      f1.setParLimits(3, 0.0, 0.1*func.getParameter(0));
-			DataFitter.fit(func,h1,"Q")
-			return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
-		}
-
-		def fits1 = (0..20).collect{makefit(f1)}
-		def bestfit = fits1.sort()[0]
-		f1.setParameters(*bestfit[1])
+//		def makefit = {func->
+//			hMean = func.getParameter(1)
+//			hRMS  = func.getParameter(2).abs()
+//			func.setRange(Math.max(hMean-2.5*hRMS, h1.getMaximumBin() - 50),Math.min(hMean+2.5*hRMS, h1.getMaximumBin() + 50))
+//      func.setParLimits(0, func.getParameter(0)*0.9,func.getParameter(0)*1.1);
+//      f1.setParLimits(3, 0.0, 0.1*func.getParameter(0));
+//			DataFitter.fit(func,h1,"Q")
+//			return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
+//		}
+//
+		//def fits1 = (0..0).collect{makefit(f1)}
+		//def bestfit = fits1.sort()[0]
+		//f1.setParameters(*bestfit[1])
 		//makefit(f1)
 
 		return f1
