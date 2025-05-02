77957d650d8efb59a77b216951619d7bffbe7226
Sangbaek <sangbaek.lee@anl.gov>
2025-05-02 19:14:34 -0400

    Do more minor cosmetic changes

--- src/main/java/org/jlab/clas/timeline/analysis/alert/alert_atof_tdc.groovy
+++ src/main/java/org/jlab/clas/timeline/analysis/alert/alert_atof_tdc.groovy
@@ -59,8 +59,8 @@ def has_data = new AtomicBoolean(false)
           }
           names.each{ name ->
             def gr = new GraphErrors(name)
-            gr.setTitle("ATOF "  + name + " " + variable.replace('_', ' '))
-            gr.setTitleY("ATOF " + name + " " + variable.replace('_', ' ') + " (ns)")
+            gr.setTitle(  name.substring(0, name.length()-17).replace('_', ' ').replace('atof', 'ATOF').replace('tdc', 'TDC') + variable.replace('_', ' '))
+            gr.setTitleY( name.substring(0, name.length()-17).replace('_', ' ').replace('atof', 'ATOF').replace('tdc', 'TDC') + variable.replace('_', ' ') + " (ns)")
             gr.setTitleX("run number")
             data.sort{it.key}.each{run,it->
               out.mkdir('/'+it.run)
