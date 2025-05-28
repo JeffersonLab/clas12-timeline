7ea33d23340651045aa38d3e80924d587dd26a44
Sangbaek <sangbaek.lee@anl.gov>
2025-05-28 17:01:54 -0400

    Correct the bad regex

--- src/main/java/org/jlab/clas/timeline/analysis/alert/alert_atof_tdc_minus_start_time_vs_tot_sector_13.groovy
+++ src/main/java/org/jlab/clas/timeline/analysis/alert/alert_atof_tdc_minus_start_time_vs_tot_sector_13.groovy
@@ -100,7 +100,7 @@ def has_data = new AtomicBoolean(false)
               if (it.containsKey(name)){
                 out.addDataSet(it[name])
                 // out.addDataSet(it['fit_'+name])
-                if (it.containsKey(variable + '_' + name)) gr.addPoint(it.run, it[variable + '_' + name], 13..13)
+                if (it.containsKey(variable + '_' + name)) gr.addPoint(it.run, it[variable + '_' + name], 0, 0)
               }
               else if (variable=="mean_tot_range1") println(String.format("run %d: %s either does not exist or does not have enough statistics.", it.run, name))
             }
