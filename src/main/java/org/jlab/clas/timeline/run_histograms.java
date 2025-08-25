1c9527383949ea1bde47d82d1104469e37e890f8
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-08-25 16:43:37 -0400

    perf: run-dependent filtering for step 1
    It seems like we have never been filtering the histogram-filling step 1,
    based on run group; we're already doing this in the analysis step 2, for
    RG-L, so we might as well do it to both.

--- src/main/java/org/jlab/clas/timeline/run_histograms.java
+++ src/main/java/org/jlab/clas/timeline/run_histograms.java
@@ -1,5 +1,7 @@
 package org.jlab.clas.timeline.histograms;
 
+import org.jlab.clas.timeline.util.RunDependentCut
+
 import java.io.*;
 import java.util.*;
 import java.text.SimpleDateFormat;
@@ -29,20 +31,23 @@ public class run_histograms {
     if(args.length>4)if(Integer.parseInt(args[4])==0)useTB=false;
     System.out.println("will process run number "+runNum+" from list "+filelist+", beam energy setting "+EB);
 
+    // get the dataset which contains this run number
+    var dataset = RunDependentCut.findDataset(runNum)
+
     //// instantiate histogramming classes
-    GeneralMon ana_mon     = new GeneralMon(runNum,outputDir,EB,useTB);
-    DCandFTOF  ana_dc_ftof = new DCandFTOF(runNum,outputDir,useTB);
-    CTOF       ana_ctof    = new CTOF(runNum,outputDir,useTB);
-    HTCC       ana_htc     = new HTCC(runNum,outputDir);
-    LTCC       ana_ltc     = new LTCC(runNum,outputDir,EB,useTB);
-    RICH       ana_rich    = new RICH(runNum,outputDir,EB,useTB);
-    CND        ana_cnd     = new CND(runNum,outputDir,useTB);
-    CVT        ana_cvt     = new CVT();
-    FT         ana_ft      = new FT(runNum,outputDir,useTB);
-    BAND       ana_band    = new BAND(runNum,outputDir,EB,useTB);
-    ALERT      ana_alert   = new ALERT(runNum,outputDir,EB,useTB);
-    helicity   helicity    = new helicity();
-    trigger    trigger     = new trigger();
+    GeneralMon ana_mon      = new GeneralMon(runNum,outputDir,EB,useTB);
+    DCandFTOF  ana_dc_ftof  = new DCandFTOF(runNum,outputDir,useTB);
+    CTOF       ana_ctof     = new CTOF(runNum,outputDir,useTB);
+    HTCC       ana_htcc     = new HTCC(runNum,outputDir);
+    LTCC       ana_ltcc     = new LTCC(runNum,outputDir,EB,useTB);
+    RICH       ana_rich     = new RICH(runNum,outputDir,EB,useTB);
+    CND        ana_cnd      = new CND(runNum,outputDir,useTB);
+    CVT        ana_cvt      = dataset != "rgl" ? new CVT() : null;
+    FT         ana_ft       = new FT(runNum,outputDir,useTB);
+    BAND       ana_band     = new BAND(runNum,outputDir,EB,useTB);
+    ALERT      ana_alert    = dataset == "rgl" ? new ALERT(runNum,outputDir,EB,useTB) : null;
+    helicity   ana_helicity = new helicity();
+    trigger    ana_trigger  = new trigger();
 
     List<String> toProcessFileNames = new ArrayList<String>();
     File file = new File(filelist);
@@ -85,19 +90,19 @@ public class run_histograms {
         DataEvent event = reader.getNextEvent();
 
         //// call each histogramming class instance's `processEvent`
-        ana_mon.processEvent(event);
-        ana_ctof.processEvent(event);
-        ana_dc_ftof.processEvent(event);
-        ana_htc.processEvent(event);
-        ana_ltc.processEvent(event);
-        ana_cnd.processEvent(event);
-        ana_cvt.processEvent(event);
-        ana_ft.processEvent(event);
-        ana_band.processEvent(event);
-        ana_alert.processEvent(event);
-        ana_rich.processEvent(event);
-        helicity.processEvent(event);
-        trigger.processEvent(event);
+        if(ana_mon!=null) ana_mon.processEvent(event);
+        if(ana_ctof!=null) ana_ctof.processEvent(event);
+        if(ana_dc_ftof!=null) ana_dc_ftof.processEvent(event);
+        if(ana_htcc!=null) ana_htcc.processEvent(event);
+        if(ana_ltcc!=null) ana_ltcc.processEvent(event);
+        if(ana_cnd!=null) ana_cnd.processEvent(event);
+        if(ana_cvt!=null) ana_cvt.processEvent(event);
+        if(ana_ft!=null) ana_ft.processEvent(event);
+        if(ana_band!=null) ana_band.processEvent(event);
+        if(ana_alert!=null) ana_alert.processEvent(event);
+        if(ana_rich!=null) ana_rich.processEvent(event);
+        if(ana_helicity!=null) ana_helicity.processEvent(event);
+        if(ana_trigger!=null) ana_trigger.processEvent(event);
 
         count++;
         if(count%10000 == 0){
@@ -116,19 +121,19 @@ public class run_histograms {
     System.out.println("Total : " + count + " events");
 
     //// call each histogramming class instance's `write`
-    ana_mon.write();
-    ana_ctof.write();
-    ana_dc_ftof.write();
-    ana_htc.write();
-    ana_ltc.write();
-    ana_cnd.write();
-    ana_cvt.write(outputDir, runNum);
-    ana_ft.write();
-    ana_band.write();
-    ana_alert.write();
-    ana_rich.write();
-    helicity.write(outputDir, runNum);
-    trigger.write(outputDir, runNum);
+    if(ana_mon!=null) ana_mon.write();
+    if(ana_ctof!=null) ana_ctof.write();
+    if(ana_dc_ftof!=null) ana_dc_ftof.write();
+    if(ana_htcc!=null) ana_htcc.write();
+    if(ana_ltcc!=null) ana_ltcc.write();
+    if(ana_cnd!=null) ana_cnd.write();
+    if(ana_cvt!=null) ana_cvt.write(outputDir, runNum);
+    if(ana_ft!=null) ana_ft.write();
+    if(ana_band!=null) ana_band.write();
+    if(ana_alert!=null) ana_alert.write();
+    if(ana_rich!=null) ana_rich.write();
+    if(ana_helicity!=null) ana_helicity.write(outputDir, runNum);
+    if(ana_trigger!=null) ana_trigger.write(outputDir, runNum);
 
   }
 }
