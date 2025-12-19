392a334d95b2276e364ac7b20a0c2b2313ddc135
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-12-19 09:56:33 -0500

    fix: use new `SplitLogManager`

--- libexec/environ.sh
+++ libexec/environ.sh
@@ -4,9 +4,6 @@
 printError()   { echo -e "\e[1;31m[ERROR]: $* \e[0m"   >&2; }
 printWarning() { echo -e "\e[1;35m[WARNING]: $* \e[0m" >&2; }
 
-# enable/disable verbose logger
-log_config=logging # set to 'logging' for quiet, or to 'debug' for verbose
-
 # get the source code directory
 [ -z "${BASH_SOURCE[0]}" ] && this_env=$0 || this_env=${BASH_SOURCE[0]}
 export TIMELINESRC=$(realpath $(dirname $this_env)/..)
@@ -24,14 +21,22 @@ fi
 # java options
 timeline_java_opts=(
   -cp "$TIMELINESRC/target/*:$TIMELINESRC/target/dependency/*"
-  -Djava.util.logging.config.file=$TIMELINESRC/data/logging/$log_config.properties
   -Xmx2048m
   -XX:+UseSerialGC
   -Djava.awt.headless=true
 )
+
+# logger options
+timeline_java_opts+=(
+  -Djava.util.logging.manager=org.jlab.logging.SplitLogManager
+  -D.level=WARNING # global log level
+  -Dorg.freehep.math.minuit.level=OFF
+  -Dorg.jlab.io.hipo=OFF
+)
+
+# export
 export TIMELINE_JAVA_OPTS="${timeline_java_opts[*]}"
 
 # cleanup vars
 unset this_env
-unset log_config
 unset timeline_java_opts
