66d7b244a061e685664f0e80d47bf8f7760f66e0
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-04-24 12:41:43 -0400

    refactor(QADB): upgrade charge timelines

---
+++ src/main/java/org/jlab/clas/timeline/histograms/ChargeMon.java
@@ -0,0 +1,16 @@
+package org.jlab.clas.timeline.histograms;
+
+import org.jlab.groot.data.TDirectory;
+import org.jlab.detector.qadb.QadbBinSequence;
+
+/**
+ * @author dilks
+ */
+public class ChargeMon {
+
+  public ChargeMon(QadbBinSequence qa_seq)
+  {
+
+  }
+
+}
