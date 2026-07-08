117c2164e0bcb4bbe312d919b93ca41ed4116113
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-07-08 11:55:45 -0400

    fix: `qadb_charge` works

--- src/main/java/org/jlab/clas/timeline/analysis/qadb/qadb.groovy
+++ src/main/java/org/jlab/clas/timeline/analysis/qadb/qadb.groovy
@@ -1,32 +1,35 @@
-package org.jlab.clas.timeline.analysis.qadb;
+package org.jlab.clas.timeline.analysis.qadb
 
-import org.jlab.detector.qadb.QadbBinSequence;
-import org.jlab.clas.timeline.util.Tools;
+import org.jlab.detector.qadb.QadbBinSequence
+import org.jlab.clas.timeline.util.Tools
 
 class qadb {
 
   private def qa_map = [:]
 
+  private def ana_qadb_charge = new qadb_charge()
+  private def ana_qadb_yield = new qadb_yield()
+
   public def start(histo_files) {
     Tools T = new Tools()
     histo_files.collect{it.replace(".hipo", ".dat")}.each { histo_file ->
-      def run = T.getRunNumberForAnalysis(hist_file);
-      qa_map[run] = new QadbBinSequence<Map>(histo_file);
+      def run = T.getRunNumberForAnalysis(histo_file)
+      qa_map[run] = new QadbBinSequence<Map>(histo_file)
     }
   }
 
   // ----------------------------------------------------------------------------------
 
   public def processRun(dir, run) {
-    qadb_charge.processRun(dir, run, qa_map);
-    qadb_yield.processRun(dir, run, qa_map);
+    ana_qadb_charge.processRun(dir, run, qa_map)
+    // ana_qadb_yield.processRun(dir, run, qa_map)
   }
 
   // ----------------------------------------------------------------------------------
 
   public def write() {
-    qadb_charge.write(qa_map);
-    qadb_yield.write(qa_map);
+    ana_qadb_charge.write(qa_map)
+    // ana_qadb_yield.write(qa_map)
   }
 
 }
